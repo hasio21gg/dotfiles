93b31e1c8facff68da869fa4ea064a1d2edfd4d5
Hideo Hashimoto <h-hasimoto@st-grp.co.jp>
2017-10-13 10:34:58 +0900

    Unix+DOS

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.netrc ~/.netrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+nkf -Lu -w ~/dotfiles/.vimrc > ~/dotfiles/.vimrc_linux
+nkf -Lu -w ~/dotfiles/.netrc > ~/dotfiles/.netrc_linux
+nkf -Lu -w ~/dotfiles/.bash_profile > ~/dotfiles/.bash_profile_linux
+nkf -Lu -w ~/dotfiles/.bashrc > ~/dotfiles/.bashrc_linux
+nkf -Lu -w ~/dotfiles/bash/git-prompt > ~/dotfiles/bash/git-prompt_linux
+nkf -Lu -w ~/dotfiles/bash/git-unyou > ~/dotfiles/bash/git-unyou_linux
 
-ln -sf ~/dotfiles/bash/git-prompt ~/.bash/git-prompt
-ln -sf ~/dotfiles/bash/git-unyou ~/.bash/git-unyou
+ln -sf ~/dotfiles/.netrc_linux ~/.netrc
+ln -sf ~/dotfiles/.vimrc_linux ~/.vimrc
+ln -sf ~/dotfiles/.bash_profile_linux ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc_linux ~/.bashrc
+
+ln -sf ~/dotfiles/bash/git-prompt_linux ~/.bash/git-prompt
+ln -sf ~/dotfiles/bash/git-unyou_linux ~/.bash/git-unyou
+
+mkdir -p ~/.vim/dein/repos/github.com/Shougo
+mkdir -p ~/.vim/rc
+nkf -Lu -w ~/dotfiles/vim/rc/dein.toml > ~/.vim/rc/dein.toml 
+nkf -Lu -w ~/dotfiles/vim/rc/dein_lazy.toml > ~/.vim/rc/dein_lazy.toml
+
+mkdir ~/vimfiles/Backup
+mkdir ~/vimfiles/script
+mkdir ~/vimfiles/Undo
